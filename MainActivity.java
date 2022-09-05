2a7754881d40775355c3a41afe60060c8430284d
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-05 18:03:46 +0545

    Main Operation added

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,8 @@
+import resources.MainOperation;
+
 public class MainActivity {
     public static void main(String[] args) {
         System.out.println("MainActivity.main()");
+        new MainOperation();
     }
 }
---
+++ resources/MainOperation.java
@@ -0,0 +1,12 @@
+package resources;
+
+import javax.swing.*;
+
+public class MainOperation extends JFrame {
+    public MainOperation() {
+        System.out.println("Operation.Operation()");
+        setVisible(true);
+        setFocusable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
